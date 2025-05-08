9a09e8924a6ce3aa28f2e252caf28489b6b9679d
Kento-umapro <kento.nishimura@umapro-jp.com>
2025-05-08 20:29:25 +0900

    Add files via upload

---
+++ manual_app_complete/pages/manuals/guide.js
@@ -0,0 +1,9 @@
+
+export default function Page() {
+  return (
+    <div style={ backgroundColor: "#27408B", minHeight: "100vh", color: "white", padding: 20 }>
+      <h1>④ 料理説明マニュアル</h1>
+      <p>ここに詳細内容が入ります。</p>
+    </div>
+  );
+}
